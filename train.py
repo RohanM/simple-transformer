0f609e01da859f0720df7d1b179770bda149a989
Rohan Mitchell <rohan@rohanmitchell.com>
2023-01-06 14:58:59 +1100

    Add option to truncate training data

--- train.py
+++ train.py
@@ -15,6 +15,7 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument('--exp-name', type=str, default=None)
     parser.add_argument('--tags', type=str, default=None)
     parser.add_argument('--seed', type=int, default=1)
+    parser.add_argument('--truncate-input', type=int, default=None)
     parser.add_argument('--window-size', type=int, default=64)
     parser.add_argument('--embedding-dim', type=int, default=256)
     parser.add_argument('--lr', type=float, default=0.001)
@@ -30,12 +31,14 @@ torch.manual_seed(args.seed)
 
 print('Reading text...')
 text = nltk.corpus.gutenberg.raw('austen-emma.txt')
+if args.truncate_input is not None:
+    text = text[:args.truncate_input]
+
 print(f'Preparing data...')
-
-text = text[:1000] # TEMP: Use a small subset of the data for testing
-
 data = Data(text, args.window_size)
 model = Model(Tokeniser().vocab_size(), args.embedding_dim)
+
+print('Training...')
 
 wandb.init(
     mode='online' if args.track else 'disabled',
@@ -48,12 +51,9 @@ wandb.init(
         'lr': args.lr,
     }
 )
-
 wandb.watch(model)
 
 trainer = Trainer(model, wandb, lr=args.lr, bs=args.batch_size)
-
-print('Training...')
 trainer.train(args.epochs, data)
 
 model.save('model.pt')
