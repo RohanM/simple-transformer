Fix loss calculation

simple_transformer/model.py
@@ -11,14 +11,14 @@ class Model(nn.Module):
         self.embedding = nn.Embedding(num_embeddings, num_embeddings)
 
     def forward(self, x: Tensor, target: Optional[Tensor] = None) -> Tuple[Tensor, Optional[Tensor]]:
-        x = self.embedding(x)
+        logits = self.embedding(x)
 
         loss = None
         if target is not None:
-            b, t, c = x.shape
-            loss = F.cross_entropy(x.view(b, c, t), target)
+            b, t, c = logits.shape
+            loss = F.cross_entropy(logits.view(b*t, c), target.view(b*t))
 
-        return x, loss
+        return logits, loss
 
     def infer_one(self, x: Tensor) -> Tensor:
         logits, _ = self(x)

tests/test_model.py
@@ -14,7 +14,7 @@ class TestModel(TestCase):
         model = Model(4)
         _, loss = model(
             tensor([[0, 1], [1, 2]], dtype=torch.long),
-            tensor([[1, 0, 0, 0], [0, 1, 0, 0]], dtype=torch.long),
+            tensor([[0, 1], [1, 2]], dtype=torch.long),
         )
         self.assertTrue(loss.item() > 0)
 